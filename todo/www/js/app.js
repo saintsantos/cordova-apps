2f28b60be6d6da8990918f9ce2c1102356e2e7ae
saintsantos <saintsantos1341@gmail.com>
2016-06-06 05:56:17 -0400

    Added taskbar button

--- todo/www/js/app.js
+++ todo/www/js/app.js
@@ -23,11 +23,33 @@ angular.module('todo', ['ionic'])
   });
 })
 
-.controller('TodoCtrl', function($scope) {
-  $scope.tasks = [
-    { title: 'Collect coins' },
-    { title: 'Eat mushrooms' },
-    { title: 'Get high enough to grab the flag' },
-    { title: 'Find the Princess' }
-  ];
+.controller('TodoCtrl', function($scope, $ionicModal) {
+  //No need for testing data anymore
+  $scope.tasks = [];
+
+  $ionicModal.fromTemplateUrl('new-task.html', function(modal) {
+    $scope.taskModal = modal;
+  }, {
+    scope: $scope,
+    animation: 'slide-in-up'
+  });
+
+  //called when the form is submitted
+  $scope.createTask = function(task) {
+    $scope.tasks.push({
+      title: task.title
+    });
+    $scope.taskModal.hide();
+    task.title="";
+  };
+
+  //Open our new task modal
+  $scope.newTask = function() {
+    $scope.taskModal.show();
+  };
+
+  //Close the new task modal
+  $scope.closeNewtask = function() {
+    $scope.taskModal.hide();
+  };
 })
